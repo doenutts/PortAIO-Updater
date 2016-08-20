2a9cf397893af1e46ee18fb34993a029e76fd02a
berbb <tintinridge@gmail.com>
2016-08-19 19:33:33 -0500

    1.0.0.4

---
+++ Source/UpdateLibVB/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    'Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MetroProgressSpinner1 = New MetroFramework.Controls.MetroProgressSpinner()
+        Me.MaterialFlatButton1 = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroProgressBar1 = New MetroFramework.Controls.MetroProgressBar()
+        Me.MetroProgressBar2 = New MetroFramework.Controls.MetroProgressBar()
+        Me.MaterialProgressBar1 = New MaterialSkin.Controls.MaterialProgressBar()
+        Me.MaterialProgressBar2 = New MaterialSkin.Controls.MaterialProgressBar()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel5 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel6 = New MetroFramework.Controls.MetroLabel()
+        Me.MaterialCheckBox1 = New MaterialSkin.Controls.MaterialCheckBox()
+        Me.MaterialCheckBox2 = New MaterialSkin.Controls.MaterialCheckBox()
+        Me.MaterialCheckBox3 = New MaterialSkin.Controls.MaterialCheckBox()
+        Me.MaterialFlatButton4 = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.SuspendLayout()
+        '
+        'MetroProgressSpinner1
+        '
+        resources.ApplyResources(Me.MetroProgressSpinner1, "MetroProgressSpinner1")
+        Me.MetroProgressSpinner1.Maximum = 100
+        Me.MetroProgressSpinner1.Name = "MetroProgressSpinner1"
+        Me.MetroProgressSpinner1.UseSelectable = True
+        '
+        'MaterialFlatButton1
+        '
+        resources.ApplyResources(Me.MaterialFlatButton1, "MaterialFlatButton1")
+        Me.MaterialFlatButton1.Depth = 0
+        Me.MaterialFlatButton1.Icon = Nothing
+        Me.MaterialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialFlatButton1.Name = "MaterialFlatButton1"
+        Me.MaterialFlatButton1.Primary = False
+        Me.MaterialFlatButton1.UseVisualStyleBackColor = True
+        '
+        'MetroLabel1
+        '
+        resources.ApplyResources(Me.MetroLabel1, "MetroLabel1")
+        Me.MetroLabel1.Name = "MetroLabel1"
+        '
+        'MetroLabel2
+        '
+        resources.ApplyResources(Me.MetroLabel2, "MetroLabel2")
+        Me.MetroLabel2.Name = "MetroLabel2"
+        '
+        'MetroProgressBar1
+        '
+        resources.ApplyResources(Me.MetroProgressBar1, "MetroProgressBar1")
+        Me.MetroProgressBar1.Name = "MetroProgressBar1"
+        '
+        'MetroProgressBar2
+        '
+        resources.ApplyResources(Me.MetroProgressBar2, "MetroProgressBar2")
+        Me.MetroProgressBar2.Name = "MetroProgressBar2"
+        '
+        'MaterialProgressBar1
+        '
+        Me.MaterialProgressBar1.Depth = 0
+        resources.ApplyResources(Me.MaterialProgressBar1, "MaterialProgressBar1")
+        Me.MaterialProgressBar1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialProgressBar1.Name = "MaterialProgressBar1"
+        '
+        'MaterialProgressBar2
+        '
+        Me.MaterialProgressBar2.Depth = 0
+        resources.ApplyResources(Me.MaterialProgressBar2, "MaterialProgressBar2")
+        Me.MaterialProgressBar2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialProgressBar2.Name = "MaterialProgressBar2"
+        '
+        'MetroLabel3
+        '
+        resources.ApplyResources(Me.MetroLabel3, "MetroLabel3")
+        Me.MetroLabel3.Name = "MetroLabel3"
+        '
+        'MetroLabel4
+        '
+        resources.ApplyResources(Me.MetroLabel4, "MetroLabel4")
+        Me.MetroLabel4.Name = "MetroLabel4"
+        '
+        'MetroLabel5
+        '
+        resources.ApplyResources(Me.MetroLabel5, "MetroLabel5")
+        Me.MetroLabel5.Name = "MetroLabel5"
+        '
+        'MetroLabel6
+        '
+        resources.ApplyResources(Me.MetroLabel6, "MetroLabel6")
+        Me.MetroLabel6.Name = "MetroLabel6"
+        '
+        'MaterialCheckBox1
+        '
+        resources.ApplyResources(Me.MaterialCheckBox1, "MaterialCheckBox1")
+        Me.MaterialCheckBox1.Depth = 0
+        Me.MaterialCheckBox1.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.MaterialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCheckBox1.Name = "MaterialCheckBox1"
+        Me.MaterialCheckBox1.Ripple = True
+        Me.MaterialCheckBox1.UseVisualStyleBackColor = True
+        '
+        'MaterialCheckBox2
+        '
+        resources.ApplyResources(Me.MaterialCheckBox2, "MaterialCheckBox2")
+        Me.MaterialCheckBox2.Depth = 0
+        Me.MaterialCheckBox2.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.MaterialCheckBox2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCheckBox2.Name = "MaterialCheckBox2"
+        Me.MaterialCheckBox2.Ripple = True
+        Me.MaterialCheckBox2.UseVisualStyleBackColor = True
+        '
+        'MaterialCheckBox3
+        '
+        resources.ApplyResources(Me.MaterialCheckBox3, "MaterialCheckBox3")
+        Me.MaterialCheckBox3.Depth = 0
+        Me.MaterialCheckBox3.MouseLocation = New System.Drawing.Point(-1, -1)
+        Me.MaterialCheckBox3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCheckBox3.Name = "MaterialCheckBox3"
+        Me.MaterialCheckBox3.Ripple = True
+        Me.MaterialCheckBox3.UseVisualStyleBackColor = True
+        '
+        'MaterialFlatButton4
+        '
+        resources.ApplyResources(Me.MaterialFlatButton4, "MaterialFlatButton4")
+        Me.MaterialFlatButton4.Depth = 0
+        Me.MaterialFlatButton4.Icon = Nothing
+        Me.MaterialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialFlatButton4.Name = "MaterialFlatButton4"
+        Me.MaterialFlatButton4.Primary = False
+        Me.MaterialFlatButton4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.MaterialCheckBox3)
+        Me.Controls.Add(Me.MaterialCheckBox2)
+        Me.Controls.Add(Me.MaterialCheckBox1)
+        Me.Controls.Add(Me.MetroLabel6)
+        Me.Controls.Add(Me.MetroLabel5)
+        Me.Controls.Add(Me.MetroLabel4)
+        Me.Controls.Add(Me.MetroLabel3)
+        Me.Controls.Add(Me.MaterialProgressBar2)
+        Me.Controls.Add(Me.MaterialProgressBar1)
+        Me.Controls.Add(Me.MetroProgressBar2)
+        Me.Controls.Add(Me.MetroProgressBar1)
+        Me.Controls.Add(Me.MetroLabel2)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Controls.Add(Me.MaterialFlatButton4)
+        Me.Controls.Add(Me.MaterialFlatButton1)
+        Me.Controls.Add(Me.MetroProgressSpinner1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Resizable = False
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MetroProgressSpinner1 As MetroFramework.Controls.MetroProgressSpinner
+    Friend WithEvents MaterialFlatButton1 As MaterialSkin.Controls.MaterialFlatButton
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroProgressBar1 As MetroFramework.Controls.MetroProgressBar
+    Friend WithEvents MetroProgressBar2 As MetroFramework.Controls.MetroProgressBar
+    Friend WithEvents MaterialProgressBar1 As MaterialSkin.Controls.MaterialProgressBar
+    Friend WithEvents MaterialProgressBar2 As MaterialSkin.Controls.MaterialProgressBar
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel5 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel6 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MaterialCheckBox1 As MaterialSkin.Controls.MaterialCheckBox
+    Friend WithEvents MaterialCheckBox2 As MaterialSkin.Controls.MaterialCheckBox
+    Friend WithEvents MaterialCheckBox3 As MaterialSkin.Controls.MaterialCheckBox
+    Friend WithEvents MaterialFlatButton4 As MaterialSkin.Controls.MaterialFlatButton
+End Class
---
+++ Source/UpdateLibVB/Form1.vb
@@ -0,0 +1,226 @@
+﻿Imports System.Net
+Imports System.Environment
+Imports MaterialDesignThemes
+Imports System.ComponentModel
+Imports System.Security.Cryptography
+Imports System.IO
+Imports System.Text
+Imports MetroFramework
+
+Public Class Form1
+    Dim PortAIOCommonGitVersion As String
+    Dim PortAIOGitVersion As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MaterialCheckBox1.Visible = True
+        MaterialCheckBox2.Visible = True
+        MaterialCheckBox1.Enabled = False
+        MaterialCheckBox2.Enabled = False
+        MaterialCheckBox3.Visible = True
+        Dim client As WebClient = New WebClient()
+        PortAIOCommonGitVersion = client.DownloadString("https://raw.githubusercontent.com/berbb/PortAIO-Updater/master/PortAIO.Common.version")
+        PortAIOGitVersion = client.DownloadString("https://raw.githubusercontent.com/berbb/PortAIO-Updater/master/PortAIO.version")
+        CheckUpdatePortAIO_Common()
+    End Sub
+
+    Private Sub CheckUpdatePortAIO_Common()
+        Dim PortAIOCommon As String = GetFolderPath(SpecialFolder.ApplicationData) + "\EloBuddy\Addons\Libraries\PortAIO.Common.dll"
+        If System.IO.File.Exists(PortAIOCommon) Then
+            Dim VersionPortCommon As FileVersionInfo = FileVersionInfo.GetVersionInfo(PortAIOCommon)
+            If VersionPortCommon.FileVersion <> PortAIOCommonGitVersion Then
+                Dim result As Integer = MetroMessageBox.Show(Me, "A new version is available, please download the new update!", "Version Check", MessageBoxButtons.OKCancel, MessageBoxIcon.Hand)
+                If result = DialogResult.Cancel Then
+                    CheckUpdatePortAIO()
+                    MetroLabel5.Text = "Lib Stataus : Outdated"
+                    MaterialCheckBox3.Visible = True
+                ElseIf result = DialogResult.OK Then
+                    DeleteFile()
+                    DownloadFile1()
+                End If
+            Else
+                MetroLabel5.Text = "Lib Stataus : UPDATED"
+                'MetroLabel3.Text = "UPDATED"
+                MaterialProgressBar1.Value = 100
+                CheckUpdatePortAIO()
+            End If
+        Else
+            MetroLabel5.Text = "Lib Stataus : Not found"
+            Dim result As Integer = MetroMessageBox.Show(Me, "PortAIO.Common.Dll doesn't exist, please press OK to download it.", "File Check", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+            If result = DialogResult.Cancel Then
+                'MessageBox.Show("Need Download To Play")
+                CheckUpdatePortAIO()
+                MaterialCheckBox3.Visible = True
+            ElseIf result = DialogResult.OK Then
+                DownloadFile1()
+            End If
+        End If
+    End Sub
+    Private Sub CheckUpdatePortAIO()
+        Dim PortAIO As String = GetFolderPath(SpecialFolder.ApplicationData) + "\EloBuddy\Addons\Libraries\PortAIO.dll"
+        If System.IO.File.Exists(PortAIO) Then
+            Dim VersionPort As FileVersionInfo = FileVersionInfo.GetVersionInfo(PortAIO)
+            If VersionPort.FileVersion <> PortAIOGitVersion Then
+                Dim result As Integer = MetroMessageBox.Show(Me, "A new version is available, please download the new update!", "Version Check", MessageBoxButtons.OKCancel, MessageBoxIcon.Hand)
+                If result = DialogResult.Cancel Then
+                    MetroLabel6.Text = "PortAIO Status : Outdated"
+                    MaterialCheckBox3.Visible = True
+                ElseIf result = DialogResult.OK Then
+                    DeleteFile()
+                    DownloadFile2()
+                End If
+            Else
+                MetroLabel6.Text = "PortAIO Status : UPDATED"
+                'MetroLabel4.Text = "downloading ..."
+                MaterialProgressBar2.Value = 100
+                MaterialCheckBox3.Visible = True
+                MetroProgressSpinner1.Visible = False
+            End If
+        Else
+            MetroLabel6.Text = "PortAIO Status : Not found"
+            If MaterialCheckBox1.Checked = False And MaterialCheckBox2.Checked = False Then
+                Dim result As Integer = MetroMessageBox.Show(Me, "PortAIO.Dll doesn't exist, please press OK to download it.", "File Check", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+                If result = DialogResult.Cancel Then
+                    MaterialCheckBox3.Visible = True
+                ElseIf result = DialogResult.OK And MaterialCheckBox3.Checked = False Then
+                    DownloadFile2() ' auto download
+                ElseIf MaterialCheckBox3.Checked Then
+                    DownloadFile2() ' manual download 
+                End If
+            ElseIf MaterialCheckBox1.Checked And MaterialCheckBox2.Checked Then
+                If MaterialCheckBox3.Checked Then
+                    DownloadFile2()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub autoupdateGitHub()
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        Dim appDataElo As String = appData + "\EloBuddy\Addons\Libraries\PortAIO.Common.dll"
+        '    md5code2 = md5_hash(appDataElo)
+    End Sub
+
+    Private Sub ShowDownloadProgress1(sender As Object, e As DownloadProgressChangedEventArgs)
+        MaterialProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub DownloadFile1()
+        MetroLabel3.Text = "downloading ..."
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        Dim appDataElo As String = appData + "\EloBuddy\Addons\Libraries\PortAIO.Common.dll"
+        Dim client As New WebClient()
+        AddHandler client.DownloadProgressChanged, AddressOf ShowDownloadProgress1
+        AddHandler client.DownloadFileCompleted, AddressOf OnDownloadComplete1
+        client.DownloadFileAsync(New Uri("https://github.com/berbb/PortAIO-Updater/raw/master/PortAIO.Common.dll"), appDataElo)
+    End Sub
+
+    Private Sub DownloadFile2()
+        MetroLabel4.Text = "downloading ..."
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        Dim appDataElo As String = appData + "\EloBuddy\Addons\Libraries\PortAIO.dll"
+        Dim client As New WebClient()
+        AddHandler client.DownloadProgressChanged, AddressOf ShowDownloadProgress2
+        AddHandler client.DownloadFileCompleted, AddressOf OnDownloadComplete2
+        client.DownloadFileAsync(New Uri("https://github.com/berbb/PortAIO-Updater/raw/master/PortAIO.dll"), appDataElo)
+    End Sub
+
+    Private Sub OnDownloadComplete1(ByVal sender As Object, ByVal e As AsyncCompletedEventArgs)
+        Dim PortAIOCommon As String = GetFolderPath(SpecialFolder.ApplicationData) + "\EloBuddy\Addons\Libraries\PortAIO.Common.dll"
+        Dim VersionPortCommon As FileVersionInfo = FileVersionInfo.GetVersionInfo(PortAIOCommon)
+        If Not e.Cancelled AndAlso e.Error Is Nothing Then
+            If VersionPortCommon.FileVersion = PortAIOCommonGitVersion Then
+                MetroLabel5.Text = "Lib Stataus : UPDATED"
+                MetroLabel3.Text = "download done.."
+                CheckUpdatePortAIO()
+            Else
+                MessageBox.Show("Download failed")
+            End If
+        End If
+    End Sub
+
+    Private Sub OnDownloadComplete2(ByVal sender As Object, ByVal e As AsyncCompletedEventArgs)
+        Dim PortAIO As String = GetFolderPath(SpecialFolder.ApplicationData) + "\EloBuddy\Addons\Libraries\PortAIO.dll"
+        Dim VersionPort As FileVersionInfo = FileVersionInfo.GetVersionInfo(PortAIO)
+        If Not e.Cancelled AndAlso e.Error Is Nothing Then
+            If VersionPort.FileVersion = PortAIOGitVersion Then
+                MetroLabel6.Text = "PortAIO Stataus : UPDATED"
+                MetroLabel4.Text = "download done.."
+                MaterialCheckBox3.Enabled = True
+            Else
+                MessageBox.Show("Download failed")
+            End If
+        End If
+    End Sub
+
+    Private Sub ShowDownloadProgress2(sender As Object, e As DownloadProgressChangedEventArgs)
+        MaterialProgressBar2.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub DeleteFile()
+        Dim appData As String = GetFolderPath(SpecialFolder.ApplicationData)
+        Dim PortAIO_Common As String = appData + "\EloBuddy\Addons\Libraries\PortAIO.Common.dll"
+        If System.IO.File.Exists(PortAIO_Common) Then
+            System.IO.File.Delete(PortAIO_Common)
+        Else
+            MsgBox("Doesn't exist", 64, "Open")
+        End If
+
+        Dim PortAIO As String = appData + "\EloBuddy\Addons\Libraries\PortAIO.dll"
+        If System.IO.File.Exists(PortAIO) Then
+            System.IO.File.Delete(PortAIO)
+        Else
+            MsgBox("Doesn't exist", 64, "Open")
+        End If
+    End Sub
+
+    Private Sub MaterialFlatButton1_Click(sender As Object, e As EventArgs) Handles MaterialFlatButton1.Click
+        If MaterialCheckBox1.Checked = True And MaterialCheckBox2.Checked = False Then
+            DownloadFile1()
+        End If
+        If MaterialCheckBox2.Checked And MaterialCheckBox1.Checked = False Then
+            DownloadFile2()
+        End If
+        If MaterialCheckBox1.Checked And MaterialCheckBox2.Checked Then
+            MetroMessageBox.Show(Me, "Please select only one.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Question)
+        End If
+        If MaterialCheckBox1.Checked = False And MaterialCheckBox2.Checked = False Then
+            MetroMessageBox.Show(Me, "Please choose the DLLs you want to download.", "None Selected", MessageBoxButtons.OK, MessageBoxIcon.Question)
+        End If
+    End Sub
+
+    Private Sub MaterialFlatButton2_Click(sender As Object, e As EventArgs) Handles MaterialFlatButton4.Click
+        Dim webAddress As String = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DZSQBFWWV9WEN"
+        Process.Start(webAddress)
+    End Sub
+
+    Private Sub MaterialCheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles MaterialCheckBox3.CheckedChanged
+        If MaterialCheckBox3.Checked Then
+            MaterialCheckBox1.Visible = True
+            MaterialCheckBox2.Visible = True
+            MaterialCheckBox1.Enabled = True
+            MaterialCheckBox2.Enabled = True
+            MaterialFlatButton1.Enabled = True
+        Else
+            MaterialFlatButton1.Enabled = False
+            MaterialCheckBox1.Enabled = False
+            MaterialCheckBox2.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub MetroLink1_Click(sender As Object, e As EventArgs)
+        Dim webAddress As String = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=DZSQBFWWV9WEN"
+        Process.Start(webAddress)
+    End Sub
+
+    Private Sub MaterialCheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles MaterialCheckBox1.CheckedChanged
+        If MaterialCheckBox1.Checked Then
+            MaterialCheckBox2.Checked = False
+        End If
+    End Sub
+
+    Private Sub MaterialCheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles MaterialCheckBox2.CheckedChanged
+        If MaterialCheckBox2.Checked Then
+            MaterialCheckBox1.Checked = False
+        End If
+    End Sub
+End Class
